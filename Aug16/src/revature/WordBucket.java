2bec15af5ff6613fdb6a4e8d237e195a5455254b
Kyle Carter <kmaxc3211@gmail.com>
2021-08-17 11:02:50 -0400

    step to clear array before returning if invalid

--- Aug16/src/revature/WordBucket.java
+++ Aug16/src/revature/WordBucket.java
@@ -7,14 +7,15 @@ public class WordBucket {
 
 	public static List<String> bucketize(String in, int size) {
 		List<String> out = new ArrayList<String>();
-		List<String> empty = new ArrayList<String>();
 		
 		String[] split = in.split(" ");
 		
 		String bucket = "";
 		for(String s : split) {
-			if(s.length() > size)
-				return empty;
+			if(s.length() > size) {
+				out.clear();
+				return out;
+			}
 			String temp = bucket + " " + s;
 			if(temp.trim().length() <= size) {
 				bucket = temp.trim();
@@ -33,6 +34,7 @@ public class WordBucket {
 	public static void main(String[] args) {
 		
 		System.out.println(bucketize("she sells sea shells by the sea", 10).toString());
+		System.out.println(bucketize("shesellssea shells by the sea", 10).toString());
 		System.out.println(bucketize("the mouse jumped over the cheese", 7).toString());
 		System.out.println(bucketize("fairy dust coated the air", 20).toString());
 		System.out.println(bucketize("a b c d e", 2).toString());
